baa606578dff317f45890b2f0c01b1a65bc68ec2
Luis Salazar <luis.salazar@bind.com.mx>
2018-05-22 18:39:33 -0500

    Versión 1.2
    Se agregaron nuevos campos en Invoice y se agregó el campo 'Adresses' en detalle de cliente.

--- api.module.ts
+++ api.module.ts
@@ -1,58 +1,23 @@
-import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { Configuration } from './configuration';
+import * as api from './api/api';
+import * as angular from 'angular';
 
-import { AccountingJournalsService } from './api/accountingJournals.service';
-import { AccountsService } from './api/accounts.service';
-import { ActivitiesService } from './api/activities.service';
-import { BankAccountsService } from './api/bankAccounts.service';
-import { CatalogsService } from './api/catalogs.service';
-import { ClientsService } from './api/clients.service';
-import { CommentsService } from './api/comments.service';
-import { InventoryService } from './api/inventory.service';
-import { InvoicesService } from './api/invoices.service';
-import { OrdersService } from './api/orders.service';
-import { ProductsService } from './api/products.service';
-import { ProspectsService } from './api/prospects.service';
-import { ProvidersService } from './api/providers.service';
-import { ServicesService } from './api/services.service';
-import { WarehousesService } from './api/warehouses.service';
-import { WebHooksService } from './api/webHooks.service';
+const apiModule = angular.module('api', [])
+.service('AccountingJournalsApi', api.AccountingJournalsApi)
+.service('AccountsApi', api.AccountsApi)
+.service('ActivitiesApi', api.ActivitiesApi)
+.service('BankAccountsApi', api.BankAccountsApi)
+.service('CatalogsApi', api.CatalogsApi)
+.service('ClientsApi', api.ClientsApi)
+.service('CommentsApi', api.CommentsApi)
+.service('InventoryApi', api.InventoryApi)
+.service('InvoicesApi', api.InvoicesApi)
+.service('OrdersApi', api.OrdersApi)
+.service('ProductsApi', api.ProductsApi)
+.service('ProspectsApi', api.ProspectsApi)
+.service('ProvidersApi', api.ProvidersApi)
+.service('ServicesApi', api.ServicesApi)
+.service('UserProfileApi', api.UserProfileApi)
+.service('WarehousesApi', api.WarehousesApi)
+.service('WebHooksApi', api.WebHooksApi)
 
-@NgModule({
-  imports:      [ CommonModule, HttpClientModule ],
-  declarations: [],
-  exports:      [],
-  providers: [
-    AccountingJournalsService,
-    AccountsService,
-    ActivitiesService,
-    BankAccountsService,
-    CatalogsService,
-    ClientsService,
-    CommentsService,
-    InventoryService,
-    InvoicesService,
-    OrdersService,
-    ProductsService,
-    ProspectsService,
-    ProvidersService,
-    ServicesService,
-    WarehousesService,
-    WebHooksService ]
-})
-export class ApiModule {
-    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
-        return {
-            ngModule: ApiModule,
-            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
-        }
-    }
-
-    constructor( @Optional() @SkipSelf() parentModule: ApiModule) {
-        if (parentModule) {
-            throw new Error('ApiModule is already loaded. Import your base AppModule only.');
-        }
-    }
-}
+export default apiModule;
